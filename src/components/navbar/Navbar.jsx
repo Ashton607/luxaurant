35bfd9146aeb57fa3cd8d94e8db7f98b60ded902
Ashton607 <portmanashton4@gmail.com>
2026-06-22 18:54:13 +0200

    navbar scroll animation added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import { useState,useEffect } from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
+ const [sticky,setSticky] = useState(false)
+
+  useEffect(() => {
+  const handleScroll = () => {
+  setSticky(window.scrollY > 0);
+  };
+  window.addEventListener('scroll', handleScroll);
+  return () => window.removeEventListener('scroll', handleScroll); // cleanup4
+  }, []);
+
+
   return (
-    <nav>
-        <h2>lux<span>au</span>rant</h2>
+    <nav  className={`${sticky ? 'blur-nav' : ''}`}>
+    <h2>lux<span>au</span>rant</h2>
       <ul>
         <li>Home</li>
         <li>Menu</li>
