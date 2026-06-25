28e23d90bde860e4367c29abcdaa8fa428c39633
Ashton607 <portmanashton4@gmail.com>
2026-06-25 11:44:03 +0200

    installed react scroll for navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import { useState,useEffect } from 'react'
 import './Navbar.css'
+import { Link } from "react-scroll";
+
 
 const Navbar = () => {
  const [sticky,setSticky] = useState(false)
@@ -17,10 +19,10 @@ const Navbar = () => {
     <nav  className={`${sticky ? 'blur-nav' : ''}`}>
     <h2>lux<span>au</span>rant</h2>
       <ul>
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Testimonials</li>
-        <li>Contact</li>
+        <li><Link to="Hero" smooth={true} duration={500}>Home</Link></li>
+        <li><Link to="Menu" smooth={true} duration={500}>Menu</Link></li>
+        <li><Link to="Testimonials" smooth={true} duration={500}>Testimonials</Link></li>
+        <li><Link to="Contact" smooth={true} duration={500}>Contact</Link></li>
       </ul>
     </nav>
   )
